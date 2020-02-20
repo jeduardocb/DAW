d95fdc9badaa1d30877f6508297e9db5575dcc30
Jose Eduardo Cadena Bernal <A01704641@itesm.mx>
2020-02-19 18:01:07 -0600

    version final

---
+++ lab4JS.js
@@ -0,0 +1,12 @@
+var numero = prompt("dame un numero");
+document.write("numero del 1 al numero");
+var aux=1;
+for(var i=1;i <= 3; i++){
+	for(var j=1;j <= numero; j++){
+		document.write(j)	
+		
+		
+	}
+	document.write("\n\r")
+}
+
