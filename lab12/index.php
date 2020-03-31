15d26362da0c78704848f6d340372ba9d39d897f
Jose Eduardo Cadena Bernal <A01704641@itesm.mx>
2020-03-30 22:30:41 -0600

    lab 13 y 12

--- lab12/index.php
+++ lab12/index.php
@@ -1,12 +1,13 @@
 <?php 
-
-	include("_header.html");  
-	include("_formulario.html");  
+  	session_start();
+  	
+ 	include("_header.html"); 
+	include("_iniciarSesion.html");
+	include("_subirImagen.html");
+	include("_preguntas.html"); 
 	include("_footer.html"); 
+?>
 
 
 
 
-
-
- ?>
