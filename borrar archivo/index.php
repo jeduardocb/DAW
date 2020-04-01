ac2033d121565ecaa048e6a642bc1fb8c96f3888
Jose Eduardo Cadena Bernal <A01704641@itesm.mx>
2020-04-01 09:47:57 -0600

    agregar archivo de prueba

---
+++ borrar archivo/index.php
@@ -0,0 +1,13 @@
+<?php 
+  	session_start();
+  	
+ 	include("_header.html"); 
+	include("_iniciarSesion.html");
+	include("_subirImagen.html");
+	include("_preguntas.html"); 
+	include("_footer.html"); 
+?>
+
+
+
+
